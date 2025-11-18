f517cafa58be6e9137716a9a29aa03e28dba7727
itsDurdenn <24308060610640@cetis61.edu.mx>
2025-11-17 17:02:02 -0700

    hecho

--- app.py
+++ app.py
@@ -22,5 +22,9 @@ def sesion():
 def politicas():
     return render_template('politicas de seguridad.html')
 
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
